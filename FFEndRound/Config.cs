f15001fdf7c50fe3a9ffa72690b1ba419d1f8f19
Rysik5318 <72207886+Rysik5318@users.noreply.github.com>
2023-01-04 22:49:07 +0400

    Add files via upload

---
+++ FFEndRound/Config.cs
@@ -0,0 +1,10 @@
+﻿namespace FFEndRound
+{
+    using System.ComponentModel;
+    public class Config
+    {
+        [Description("Plugin Enabled?")]
+        public bool IsEnabled { get; set; } = true;
+        public string Broadcast { get; set; } = "<color=red>FF</color> <color=green>ON</color>!";
+    }
+}
---
+++ FFEndRound/EventHandlers.cs
@@ -0,0 +1,20 @@
+﻿namespace InfinityAmmo
+{
+    using PluginAPI.Core;
+    using PluginAPI.Core.Attributes;
+    using PluginAPI.Enums;
+    public class EventHandlers
+    {
+        [PluginEvent(ServerEventType.RoundEnd)]
+        public void OnRoundEnded(RoundSummary.LeadingTeam leadingTeam)
+        {
+            Server.SendBroadcast(FFEndRound.FFEndRound.Instance.Config.Broadcast, 30);
+            Server.FriendlyFire = true;
+        }
+        [PluginEvent(ServerEventType.RoundStart)]
+        public void OnRoundStart()
+        {
+            Server.FriendlyFire = false;
+        }
+    }
+}
