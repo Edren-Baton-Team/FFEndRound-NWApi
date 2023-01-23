c15d5da5aff81c41bfe94bf9643ee21342398516
Rysik5318 <72207886+Rysik5318@users.noreply.github.com>
2023-01-23 17:19:22 +0400

    Add files via upload

---
+++ EventHandlers.cs
@@ -0,0 +1,20 @@
+﻿namespace FFEndRound
+{
+    using PluginAPI.Core;
+    using PluginAPI.Core.Attributes;
+    using PluginAPI.Enums;
+    public class EventHandlers
+    {
+        [PluginEvent(ServerEventType.RoundEnd)]
+        void OnRoundEnded(RoundSummary.LeadingTeam leadingTeam)
+        {
+            Server.SendBroadcast(FFEndRound.Instance.Config.Broadcast, FFEndRound.Instance.Config.BroadcastTime);
+            Server.FriendlyFire = true;
+        }
+        [PluginEvent(ServerEventType.RoundRestart)]
+        public void OnRestart()
+        {
+            Server.FriendlyFire = false;
+        }
+    }
+}
