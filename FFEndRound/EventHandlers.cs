1206c4f8dd3efb417061b79504b2e9f5ae83a213
Rysik5318 <72207886+Rysik5318@users.noreply.github.com>
2023-02-25 16:26:38 +0400

    Add files via upload

---
+++ FFEndRound/EventHandlers.cs
@@ -0,0 +1,18 @@
+﻿namespace FFEndRound
+{
+    using PluginAPI.Core;
+    using PluginAPI.Core.Attributes;
+    using PluginAPI.Enums;
+
+    public class EventHandlers
+    {
+        [PluginEvent(ServerEventType.RoundEnd)]
+        public void OnRoundEnded(Server server)
+        {
+            Server.SendBroadcast(FFEndRound.Instance.Config.Broadcast, FFEndRound.Instance.Config.BroadcastTime);
+            Server.FriendlyFire = true;
+        }
+        [PluginEvent(ServerEventType.RoundRestart)]
+        public void OnRestart() => Server.FriendlyFire = false;
+    }
+}
---
+++ FFEndRound/Plugin.cs
@@ -0,0 +1,29 @@
+﻿using PluginAPI.Core;
+using PluginAPI.Core.Attributes;
+using PluginAPI.Enums;
+using PluginAPI.Events;
+
+namespace FFEndRound
+{
+    public class FFEndRound
+    {
+        public const string Name = "FFEndRound";
+        public const string Version = "v1.0.2";
+        public const string Author = "Rysik5318";
+        public static FFEndRound Instance { get; private set; }
+        [PluginConfig("config.yml")]
+        public Config Config;
+        [PluginPriority(LoadPriority.Highest)]
+        [PluginEntryPoint(Name, Version, "FFEndRound!", Author)]
+        void LoadPlugin()
+        {
+            if (!Config.IsEnabled)
+                return;
+            Instance = this;
+            EventManager.RegisterEvents<EventHandlers>(this);
+            Log.Raw($"<color=blue>Loading {Name} {Version} by {Author}</color>");
+            var handler = PluginHandler.Get(this);
+            handler.SaveConfig(this, nameof(Config));
+        }
+    }
+}
